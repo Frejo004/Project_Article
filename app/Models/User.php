0f0a0950d2a494786ea133def9eb76b8332da00d
Frejus DASSI <frejohope@gmail.com>
2024-12-02 19:24:46 +0100

    ajout de 'admin' sur le model User

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'admin',
     ];
 
     /**
@@ -44,5 +45,11 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    // Un utilisateur écrit plusieurs articles
+    public function articles()
+    {
+        return $this->hasMany(Article::class);
     }
 }
