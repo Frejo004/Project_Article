6bd30b8679f8952cb9e5cd8026a6180456268a22
Frejus DASSI <frejohope@gmail.com>
2024-12-02 19:19:57 +0100

    les Routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,26 @@
 <?php
 
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\LoginAuth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', [ArticleController::class, 'index']);
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+
+Route::get('/login', [AuthController::class, 'login']);
+Route::post('/login', [LoginAuth::class, 'authenticate']);
+
+Route::get('/sign-up', [AuthController::class, 'signup']);
+Route::post('/sign-up', [AuthController::class, 'sign_up'])->name('sign-up');
+
+
+
+Route::get('/article', [ArticleController::class, 'create']);
+Route::post('/article', [ArticleController::class, 'store']);
+
+Route::get('/show', [ArticleController::class, 'show'])->name('articles.show');
+
+Route::get('/index', [ArticleController::class, 'index'])->name('articles.download');
